adc654d7cf23e9cc365c3e3863c0200e6518bd0b
unknown <migarate@uc.cl>
2014-09-01 17:17:04 -0400

    Walking

---
+++ Project/Assets/TrapMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapMovement : MonoBehaviour {
+
+	public float max_height;
+	public float min_height;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y >= max_height)
+				rigidbody.velocity = transform.forward * -speed;
+		if (transform.position.y <= min_height)
+			rigidbody.velocity = transform.forward * speed;
+
+
+	}
+}
--- Project/Assets/Walking.cs
+++ Project/Assets/Walking.cs
@@ -2,17 +2,31 @@
 using System.Collections;
 
 public class Walking : MonoBehaviour {
-
-
+	
+	
 	private Animator anim;
-	public bool change;
+	public float speed;
+	
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		anim.SetBool ("BeWalking", change);
+	
+	
+	
+	
+	void FixedUpdate() 
+	{
+		float movHorizontal = Input.GetAxis ("Horizontal")*speed;
+		//float movVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (movHorizontal,rigidbody.velocity.y, 0.0f );
+		rigidbody.velocity = movement;
+		if (movHorizontal != 0)
+			anim.SetBool("BeWalking",true);
+		else
+			anim.SetBool("BeWalking",false);
+		
 	}
 }
