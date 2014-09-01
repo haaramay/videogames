cc93b7110747538409e43716a13e0556b07d633e
haaramay <haaramay@uc.cl>
2014-09-01 15:09:56 -0400

    First Commit

---
+++ Project/Assets/Walking.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walking : MonoBehaviour {
+
+
+	private Animator anim;
+	public bool change;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("BeWalking", change);
+	}
+}
